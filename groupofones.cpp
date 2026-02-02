02101bbc877e632843dfeec5bc41b31baf007559
Deniza <kyzyldeniz@gmail.com>
2026-02-02 22:25:04 +0100

    second hw task wip

---
+++ groupofones.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>  // second ever code in c++ wow
+using namespace std;
+
+int n; //number of elements
+vector<int> v; //our numbers go here
+int current = 0;
+int best = 0;
+
+
+int main() {
+    cin >> n;
+    v.reserve(n);
+
+    int i=0;
+    while (i<n) {
+        int arrEl;
+        cin >> arrEl;
+        v.push_back(arrEl);
+        i++;
+    }
+
+    for (int meow=0; meow<n; meow++) {
+        if (v[meow] == 1) {
+            current = current + 1;
+        }
+        else if (v[meow]==0) {
+            if (current > best) {
+                best = current;
+                current = 0;
+            
+            current = 0;
+            }
+        else {
+            cout << "zeroes and ones only bucko"; 
+            return -1;
+        }
+    
+    cout << best;
+        }
+    
+}}
