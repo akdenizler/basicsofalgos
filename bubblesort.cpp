b62514b5f1de5a21134e3d805727788d18e34a34
Deniza <kyzyldeniz@gmail.com>
2026-02-03 15:32:44 +0100

    babble sort :)

---
+++ bubblesort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int n; //number of elements  in the arrau 
+vector<int> v; 
+
+void readinginput(){
+    int arrEl;
+    for (int i=0;i<n; i++) { //reading array elements
+    cin >> arrEl;
+    v.push_back(arrEl); 
+    }
+
+}
+
+int main(){ // babble sort :)
+    cin >> n;
+        for (int meow=0; meow<n; meow++) {
+            for (int bark=0; bark<n-meow; bark++) {
+                if (v[bark] > v[bark+1]) {
+                std::swap(v[bark], v[bark + 1]);
+        }
+
+    }
+   }
+    for (int x : v) cout << x << " ";
+
+
+
+
+}
+
+
